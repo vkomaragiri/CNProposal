11bebd5e556771394ebb84c1e9951cfe5c6ae04a
Vibhav Gogate <vibhav.gogate@utdallas.edu>
2019-10-09 02:27:21 -0500

    Changed

--- include/HyperParameters.h
+++ include/HyperParameters.h
@@ -17,6 +17,7 @@ struct HyperParameters{
     static int num_components;
     static int interval_for_structure_learning;
     static ldouble  mi_low_limit;
+    static int num_samples;
     HyperParameters()= default;
     static void setNumIterationsEM(int x){if (x<100000) num_iterations_em=x;}
     static void setIntervalForStructureLearning(int x){interval_for_structure_learning=x;}
